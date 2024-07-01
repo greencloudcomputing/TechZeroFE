aabbdccb2acf9fdfd5e4efc95a60304bff1024ba
manonatallye <manon@allye.com>
2024-07-01 13:54:44 +0100

    API call first test

--- function-carbon-intensity-frontend/src/views/AboutView.vue
+++ function-carbon-intensity-frontend/src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ info }} USD</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((response) => (this.info = response))
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
